b59ee8107ee2b9231dae2e20f7bd3b15d5c22248
nnashiki <n.nashiki.work@gmail.com>
2018-12-01 20:19:36 +0900

    update: 表示数を増やした

--- jupyter/config/jovyan/.ipython/profile_default/startup/00_init.py
+++ jupyter/config/jovyan/.ipython/profile_default/startup/00_init.py
@@ -11,6 +11,9 @@ db = pymysql.connect(host=  '172.17.0.2',
                      cursorclass=pymysql.cursors.DictCursor)
 
 try:
+    pd.set_option("display.max_colwidth", 255)
+    pd.set_option("display.max_columns", 150)
+
     bb_df = pd.read_sql_query(
             "SELECT * from batter",db)
 
