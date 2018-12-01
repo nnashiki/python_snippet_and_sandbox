3b332c2f1e7e62ad21b9f9aafea9ab730fb3cab2
nnashiki <n.nashiki.work@gmail.com>
2018-12-01 20:12:00 +0900

    update:  dfにデータが入っていることを確認出来た

--- jupyter/config/jovyan/.ipython/profile_default/startup/00_init.py
+++ jupyter/config/jovyan/.ipython/profile_default/startup/00_init.py
@@ -1,1 +1,18 @@
+import pymysql
+import pandas as pd
 
+
+# 野球情報の取得
+db = pymysql.connect(host=  '172.17.0.2',
+                     user='baseball_user',
+                     password='baseball_pass',
+                     db='baseball_db',
+                     charset='utf8',
+                     cursorclass=pymysql.cursors.DictCursor)
+
+try:
+    bb_df = pd.read_sql_query(
+            "SELECT * from batter",db)
+
+finally:
+    db.close()
