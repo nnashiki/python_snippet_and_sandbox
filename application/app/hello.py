d4013bc77bfd40b69ae38555007987e43bee860c
nnashiki <n.nashiki.work@gmail.com>
2019-01-13 00:33:32 +0900

    update: hello worldできた

--- application/app/hello.py
+++ application/app/hello.py
@@ -2,9 +2,11 @@ import responder
 
 api = responder.API()
 
+
 @api.route("/")
 def hello_world(req, resp):
     resp.text = "hello, world!"
 
+
 if __name__ == '__main__':
-    api.run(port=8080)
+    api.run(address="0.0.0.0")
