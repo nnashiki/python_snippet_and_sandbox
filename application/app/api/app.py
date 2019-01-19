b246f63b662a651749a2d4204effd8ef06432211
nnashiki <n.nashiki.work@gmail.com>
2019-01-20 08:17:33 +0900

    update:  playerを追加

--- application/app/api/app.py
+++ application/app/api/app.py
@@ -8,5 +8,10 @@ def hello_world(req, resp):
     resp.text = "hello, world!"
 
 
+@api.route("/player/{id}")
+def get_player_info(req, resp, *, id):
+    resp.text = f"hello, world! {id}"
+
+
 if __name__ == '__main__':
     api.run(address="0.0.0.0")
