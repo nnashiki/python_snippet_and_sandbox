6f60541d88fa52a1d793956b191f90061ffecae5
nnashiki <n.nashiki.work@gmail.com>
2019-01-20 09:14:59 +0900

    update: dbから引く処理を追加してみる

--- application/app/api/app.py
+++ application/app/api/app.py
@@ -1,4 +1,5 @@
 import responder
+import pymysql
 
 api = responder.API()
 
@@ -10,7 +11,22 @@ def hello_world(req, resp):
 
 @api.route("/player/{id}")
 def get_player_info(req, resp, *, id):
-    resp.text = f"hello, world! {id}"
+    conn = pymysql.connect(host='172.17.0.2',
+                           user='baseball_user',
+                           password='baseball_pass',
+                           db='baseball_db',
+                           charset='utf8',
+                           cursorclass=pymysql.cursors.DictCursor)
+
+    try:
+        with conn.cursor() as cursor:
+            sql = "SELECT id from batter"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            print(result)
+    finally:
+        conn.close()
+    resp.text = result
 
 
 if __name__ == '__main__':
