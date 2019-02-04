8ebb6c5be503eea348005608a5871bc042f19e87
nnashiki <n.nashiki.work@gmail.com>
2019-02-04 14:26:03 +0900

    update: 選手毎に表示できる

--- jupyter/note/Untitled.py
+++ jupyter/note/Untitled.py
@@ -10,8 +10,20 @@ bb_df
 # In[ ]:
 
 
-sakamoto_df = bb_df[bb_df['name'] == '坂本 勇人'][['h','year']]
+select_colum = ['year','h','name']
+player_list = ['坂本 勇人','山田 哲人']
+
+sakamoto_df = bb_df[bb_df['name'].isin(player_list)][select_colum]
 display(sakamoto_df)
+
+
+# In[ ]:
+
+
+df_list= [bb_df[bb_df['name'] == player][select_colum] for player in player_list]
+
+for df in df_list:
+    display(df)
 
 
 # In[ ]:
@@ -20,20 +32,31 @@ display(sakamoto_df)
 from bokeh.io import output_notebook, show
 from bokeh.plotting import figure, output_file, show
 from bokeh.layouts import column
-from bokeh.models import DataSource,RangeTool,HoverTool
+from bokeh.models import DataSource, RangeTool, HoverTool
 import bokeh.palettes as bp
 output_notebook()
 import pandas as pd
 import numpy as np
 
+p = figure(
+    title='タイトル',  # タイトルを入力
+    x_axis_label='timestamp',  # x軸のラベル
+    y_axis_label='EP',  # y軸のラベル
+    width=1000,
+    height=500,  # グラフの幅と高さの指定
+)
 
-p = figure(title='タイトル',         # タイトルを入力
-           x_axis_label='timestamp', # x軸のラベル
-           y_axis_label='EP',        # y軸のラベル
-           width=1000 ,height=500,     # グラフの幅と高さの指定
-          )
+select_colum = ['year','h','name']
+player_list = ['坂本 勇人','山田 哲人']
 
-p.line(x=sakamoto_df['year'],y=sakamoto_df['h'],color='red',legend='sakamoto')
+df_list= [bb_df[bb_df['name'] == player][select_colum] for player in player_list]
+
+for df in df_list:
+    df = df.sort_values('year')
+    p.line(
+    x=df['year'], y=df['h'], color='red', legend=df['name'].unique()[0])
+
+
 
 show(column(p))
 
